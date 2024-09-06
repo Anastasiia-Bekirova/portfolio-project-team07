65d445f5e655055f25525d94fc9448ec36ef23f6
Kateryna <koliesnikova.kateryna@gmail.com>
2024-09-07 01:12:28 +0300

    all-except-adaptation

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -17,14 +17,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const accordionElements = document.querySelectorAll('.about-me-acc-el');
 
     accordionElements.forEach(el => {
+      const arrowDown = el.querySelector('.arr-down');
+      const arrowUp = el.querySelector('.arr-up');
+      
       if (el !== activeElement) {
         el.style.marginBottom = '0';
+        arrowDown.classList.remove('is-hidden');
+        arrowUp.classList.add('is-hidden');
       }
     });
 
     if (activeElement) {
       const activePanel = activeElement.querySelector('.about-me-acc-el-descr-frame');
-      activeElement.style.marginBottom = `${activePanel.scrollHeight}px`;
+      const activeArrowDown = activeElement.querySelector('.arr-down');
+      const activeArrowUp = activeElement.querySelector('.arr-up');
+      
+      activePanel.style.paddingBottom = `${activePanel.scrollHeight}px`;
+      activeArrowDown.classList.add('is-hidden');
+      activeArrowUp.classList.remove('is-hidden');
     }
   };
 
