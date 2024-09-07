304356493008a8e45c5ad93e8af15099e1e5549a
Serhii01010101 <jobjob2109@gmail.com>
2024-09-07 18:42:58 +0300

    change overfloy

--- src/js/projects.js
+++ src/js/projects.js
@@ -5,8 +5,7 @@ import 'swiper/css/navigation';
 
 const swiper = new Swiper('.project-swiper-container', {
   modules: [Navigation, Keyboard],
-  slidesPerView: 1, // Показывать только один слайд за раз
-  spaceBetween: 0, // Убедитесь, что между слайдами нет отступов
+
   navigation: {
     nextEl: '.projects-button-next',
     prevEl: '.projects-button-prev',
